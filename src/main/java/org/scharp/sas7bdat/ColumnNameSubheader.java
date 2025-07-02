de29da674429a6d650a4e142bfb571ea692d20c0
David Costanzo <dcostanz@scharp.org>
2025-07-01 18:02:22 -0700

    Refactoring: Move ColumnNameSubheader from Sas7bdatWriter to its own file.
    This improves encapsulation, since nested classes can't have members that aren't visible to the outer class.

---
+++ src/main/java/org/scharp/sas7bdat/ColumnNameSubheader.java
@@ -0,0 +1,78 @@
+package org.scharp.sas7bdat;
+
+import java.util.List;
+
+import static org.scharp.sas7bdat.WriteUtil.write2;
+import static org.scharp.sas7bdat.WriteUtil.write4;
+import static org.scharp.sas7bdat.WriteUtil.write8;
+
+/**
+ * A subheader that contains the names of columns
+ */
+class ColumnNameSubheader extends Subheader {
+
+    /**
+     * No subheader can be larger than Short.MAX_VALUE, so we only have room for (Short.MAX_VALUE - 28) / 8 = 4092
+     * variables in each one.  However, datasets which SAS creates puts a maximum of 4089 in each subheader.
+     */
+    private static final int MAX_VARIABLES_PER_SUBHEADER = 4089;
+
+    final List<Variable> variables;
+    private final ColumnText columnText;
+
+    ColumnNameSubheader(List<Variable> variables, int variablesOffset, ColumnText columnText) {
+        // Determine how many variables, starting at offset, this subheader will hold.
+        assert variablesOffset < variables.size();
+        int variablesRemaining = variables.size() - variablesOffset;
+        int totalVariablesInSubheader = Math.min(variablesRemaining, MAX_VARIABLES_PER_SUBHEADER);
+
+        // Copy the variables
+        int limit = variablesOffset + totalVariablesInSubheader;
+        this.variables = variables.subList(variablesOffset, limit);
+
+        this.columnText = columnText;
+
+        assert size() <= Short.MAX_VALUE : "Too many variables in ColumnNameSubheader";
+    }
+
+    @Override
+    int size() {
+        return variables.size() * 8 + 28;
+    }
+
+    @Override
+    void writeSubheader(byte[] page, int subheaderOffset) {
+        write8(page, subheaderOffset, SIGNATURE_COLUMN_NAME); // signature
+
+        write8(page, subheaderOffset + 8, (short) (size() - 20)); // remaining bytes in subheader
+
+        int offsetWithinSubheader = 16;
+        for (Variable variable : variables) {
+            // Locate the text subheader that has this variable's name.
+            final String variableName = variable.name();
+
+            // subheader index/offset/length of name.
+            columnText.writeTextLocation(page, subheaderOffset + offsetWithinSubheader, variableName);
+
+            // padding
+            write2(page, subheaderOffset + offsetWithinSubheader + 6, (short) 0);
+
+            offsetWithinSubheader += 8;
+        }
+
+        // There is some padding at the end.
+        assert size() == offsetWithinSubheader + 12;
+        write4(page, subheaderOffset + offsetWithinSubheader, 0);
+        write8(page, subheaderOffset + offsetWithinSubheader + 4, 0);
+    }
+
+    @Override
+    byte typeCode() {
+        return SUBHEADER_TYPE_B;
+    }
+
+    @Override
+    byte compressionCode() {
+        return COMPRESSION_UNCOMPRESSED;
+    }
+}
---
+++ src/test/java/org/scharp/sas7bdat/ColumnNameSubheaderTest.java
@@ -0,0 +1,217 @@
+package org.scharp.sas7bdat;
+
+import org.junit.jupiter.api.Test;
+import org.scharp.sas7bdat.Sas7bdatWriter.Sas7bdatUnix64bitMetadata;
+import org.scharp.sas7bdat.Sas7bdatWriter.Sas7bdatUnix64bitVariables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.scharp.sas7bdat.Subheader.COMPRESSION_UNCOMPRESSED;
+import static org.scharp.sas7bdat.Subheader.SUBHEADER_TYPE_B;
+
+/** Unit tests for {@link ColumnNameSubheader}. */
+public class ColumnNameSubheaderTest {
+
+    private static ColumnText newColumnText(List<Variable> variableList) {
+        PageSequenceGenerator pageSequenceGenerator = new PageSequenceGenerator();
+        Sas7bdatUnix64bitVariables variables = new Sas7bdatUnix64bitVariables(variableList);
+        Sas7bdatUnix64bitMetadata metadata = new Sas7bdatUnix64bitMetadata(pageSequenceGenerator, 0x10000, variables);
+        ColumnText columnText = new ColumnText(metadata);
+
+        // Populate the column text with the strings from the variableList.
+        for (Variable variable : variableList) {
+            columnText.add(variable.name());
+        }
+
+        return columnText;
+    }
+
+    @Test
+    void testTypeCode() {
+        List<Variable> variables = List.of(new Variable(
+            "TEXT",
+            1,
+            VariableType.CHARACTER,
+            20,
+            "A label",
+            Format.UNSPECIFIED,
+            new Format("$", 10),
+            StrictnessMode.SAS_ANY));
+        ColumnText columnText = newColumnText(variables);
+
+        ColumnNameSubheader columnNameSubheader = new ColumnNameSubheader(variables, 0, columnText);
+        assertEquals(SUBHEADER_TYPE_B, columnNameSubheader.typeCode());
+    }
+
+    @Test
+    void testCompressionCode() {
+        List<Variable> variables = List.of(new Variable(
+            "TEXT",
+            1,
+            VariableType.CHARACTER,
+            20,
+            "A label",
+            Format.UNSPECIFIED,
+            new Format("$", 10),
+            StrictnessMode.SAS_ANY));
+        ColumnText columnText = newColumnText(variables);
+
+        ColumnNameSubheader columnNameSubheader = new ColumnNameSubheader(variables, 0, columnText);
+        assertEquals(COMPRESSION_UNCOMPRESSED, columnNameSubheader.compressionCode());
+    }
+
+    @Test
+    void testSingleVariable() {
+        List<Variable> variables = List.of(new Variable(
+            "MY_VAR",
+            1,
+            VariableType.NUMERIC,
+            8,
+            "A label",
+            new Format("$OUTPUT", 8, 2),
+            new Format("$INPUT", 9, 6),
+            StrictnessMode.SAS_ANY));
+
+        ColumnText columnText = newColumnText(variables);
+
+        ColumnNameSubheader columnNameSubheader = new ColumnNameSubheader(variables, 0, columnText);
+
+        final byte[] expectedSubheaderData = new byte[] {
+            -1, -1, -1, -1, -1, -1, -1, -1,  // signature
+            16, 0, 0, 0, 0, 0, 0, 0, // size of data
+
+            0, 0, // column text subheader index of name
+            8, 0, // offset of name in subheader
+            6, 0, // name length
+            0, 0, // padding
+
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 // 12 bytes of padding at end
+        };
+
+        int size = columnNameSubheader.size();
+        assertEquals(expectedSubheaderData.length, columnNameSubheader.size());
+
+        // Get the subheader with a non-zero offset.
+        int offset = 10;
+        byte[] actualSubheaderData = new byte[size + offset];
+        columnNameSubheader.writeSubheader(actualSubheaderData, offset);
+
+        // Determine the expected return value.
+        byte[] expectedArray = new byte[expectedSubheaderData.length + offset];
+        System.arraycopy(expectedSubheaderData, 0, expectedArray, offset, expectedSubheaderData.length);
+
+        // Confirm that writeSubheader() wrote the data to the expected location.
+        assertArrayEquals(expectedArray, actualSubheaderData);
+    }
+
+    @Test
+    void testSublistOfVariables() {
+        List<Variable> variables = List.of(
+            new Variable(
+                "BEFORE",
+                1, // variable 1
+                VariableType.NUMERIC,
+                8,
+                "A label",
+                new Format("$OUTPUT", 8, 2),
+                new Format("$INPUT", 9, 6),
+                StrictnessMode.SAS_ANY),
+            new Variable(
+                "TEXT1",
+                2, // variable 2
+                VariableType.CHARACTER,
+                256,
+                "label",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY),
+            new Variable(
+                "LONGTEXT2", //
+                3, // variable 3
+                VariableType.CHARACTER,
+                101,
+                "label",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY),
+            new Variable(
+                "NUMBER 1", //
+                4, // variable 4
+                VariableType.NUMERIC,
+                8,
+                "label",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY));
+
+        ColumnText columnText = newColumnText(variables);
+
+        ColumnNameSubheader columnNameSubheader = new ColumnNameSubheader(variables, 1, columnText);
+
+        // Write the contents of the subheader to a byte array.
+        final byte[] expectedSubheaderData = new byte[] {
+            -1, -1, -1, -1, -1, -1, -1, -1,  // signature
+            32, 0, 0, 0, 0, 0, 0, 0, // size of data
+
+            // variable 2 is named "TEXT1"
+            0, 0, // column text subheader index of name
+            16, 0, // offset of name in subheader
+            5, 0, // name length
+            0, 0, // padding
+
+            // variable 3 is named "LONGTEXT2"
+            0, 0, // column text subheader index of name
+            24, 0, // offset of name in subheader
+            9, 0, // name length
+            0, 0, // padding
+
+            // variable 4 is named "NUMBER 1"
+            0, 0, // column text subheader index of name
+            36, 0, // offset of name in subheader
+            8, 0, // name length
+            0, 0, // padding
+
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 // 12 bytes of padding at end
+        };
+
+        int size = columnNameSubheader.size();
+        assertEquals(expectedSubheaderData.length, columnNameSubheader.size());
+
+        // Confirm that writeSubheader() writes the expected data.
+        byte[] actualSubheaderData = new byte[size];
+        columnNameSubheader.writeSubheader(actualSubheaderData, 0);
+        assertArrayEquals(expectedSubheaderData, actualSubheaderData);
+    }
+
+    @Test
+    void testMaximumNumberOfVariables() {
+        // Create a list with the most number of variables that a SAS7BDAT can have.
+        List<Variable> variables = new ArrayList<>(Short.MAX_VALUE);
+        for (int variableNumber = 1; variableNumber <= Short.MAX_VALUE; variableNumber++) {
+            Variable variable = new Variable(
+                "VARIABLE_" + variableNumber,
+                variableNumber, // variable number
+                VariableType.NUMERIC,
+                8,
+                "label is ignored",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY);
+            variables.add(variable);
+        }
+
+        ColumnText columnText = newColumnText(variables);
+        ColumnNameSubheader columnNameSubheader = new ColumnNameSubheader(variables, 0, columnText);
+
+        int size = columnNameSubheader.size();
+        assertEquals(32740, columnNameSubheader.size());
+
+        // Confirm that writeSubheader() can write the data.
+        // The data is too long to check.
+        byte[] actualSubheaderData = new byte[size];
+        columnNameSubheader.writeSubheader(actualSubheaderData, 0);
+    }
+}
