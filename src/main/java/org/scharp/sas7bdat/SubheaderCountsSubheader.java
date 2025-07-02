4ef85db75f9d07af93ec80a3020fc14ac003a7d3
David Costanzo <dcostanz@scharp.org>
2025-07-02 06:31:54 -0700

    Refactoring: Move SubheaderCountsSubheader from Sas7bdatWriter to its own file.
    This improves encapsulation, since nested classes can't have members that aren't visible to the outer class.

---
+++ src/main/java/org/scharp/sas7bdat/SubheaderCountsSubheader.java
@@ -0,0 +1,140 @@
+package org.scharp.sas7bdat;
+
+import org.scharp.sas7bdat.Sas7bdatWriter.Sas7bdatUnix64bitMetadata;
+
+import java.util.Collection;
+
+import static org.scharp.sas7bdat.WriteUtil.write8;
+
+/**
+ * A subheader that contains some additional information about the repeatable subheaders.
+ */
+class SubheaderCountsSubheader extends Subheader {
+    /**
+     * The number of bytes in a SubheaderCountsSubheader.
+     */
+    private static final int PAGE_SIZE = 600;
+
+    private final Collection<Variable> variables;
+    private final Sas7bdatUnix64bitMetadata metadata;
+
+    SubheaderCountsSubheader(Collection<Variable> variables, Sas7bdatUnix64bitMetadata metadata) {
+        this.variables = variables;
+        this.metadata = metadata; // this is filled in later by the caller
+    }
+
+    private <T extends Subheader> void writeSubheaderInformation(byte[] page, int offset, Class<T> clazz,
+        long signature) {
+        // Determine the location of the first/last subheader of the requested type.
+        // These values are initialized to something that means "not found".
+        short pageOfFirstAppearance = 0;
+        short positionOfFirstAppearance = 0;
+        short pageOfLastAppearance = 0;
+        short positionOfLastAppearance = 0;
+
+        if (clazz != null) {
+            short currentPageNumber = 0; // impossible number
+            short subheaderPositionOnPage = 0;
+            for (Subheader subheader : metadata.subheaders) {
+                short subheaderPageNumber = metadata.subheaderLocations.get(subheader).shortValue();
+                if (subheaderPageNumber == currentPageNumber) {
+                    // This subheader is on the same page as the previous subheader.
+                    subheaderPositionOnPage++;
+                } else {
+                    // This is the first subheader is on a new page.
+                    assert currentPageNumber + 1 == subheaderPageNumber : "skipped a page";
+                    currentPageNumber = subheaderPageNumber;
+                    subheaderPositionOnPage = 1;
+                }
+
+                if (clazz.isInstance(subheader)) {
+                    // If this is the first time we've seen this subheader type, note it as the first appearance.
+                    if (pageOfFirstAppearance == 0) {
+                        pageOfFirstAppearance = subheaderPageNumber;
+                        positionOfFirstAppearance = subheaderPositionOnPage;
+                    }
+
+                    // Always log the last occurrence.
+                    pageOfLastAppearance = subheaderPageNumber;
+                    positionOfLastAppearance = subheaderPositionOnPage;
+                }
+            }
+        }
+
+        // Write the information to the page.
+        write8(page, offset + 0, signature);
+        write8(page, offset + 8, pageOfFirstAppearance);
+        write8(page, offset + 16, positionOfFirstAppearance);
+        write8(page, offset + 24, pageOfLastAppearance);
+        write8(page, offset + 32, positionOfLastAppearance);
+    }
+
+    @Override
+    int size() {
+        return PAGE_SIZE;
+    }
+
+    @Override
+    void writeSubheader(byte[] page, int subheaderOffset) {
+        write8(page, subheaderOffset, SIGNATURE_SUBHEADER_COUNTS); // signature
+
+        // The next field appears to be the maximum size of the ColumnTextSubheader or ColumnAttributesSubheader
+        // blocks, as reported at their offset 8.  This doesn't include the signature or the padding.
+        // This may also include all variable-length subheaders, but the rest would be smaller.
+        // This may be a performance hint for how large a buffer to allocate when reading
+        // the data.  However, setting this to small or negative value doesn't prevent SAS
+        // from reading the dataset, so its value may be ignored.
+        int maxTextBlockSize = 0;
+        for (Subheader subheader : metadata.subheaders) {
+            if (subheader instanceof ColumnTextSubheader) {
+                maxTextBlockSize = Math.max(maxTextBlockSize, ((ColumnTextSubheader) subheader).sizeOfData());
+            } else if (subheader instanceof ColumnAttributesSubheader) {
+                maxTextBlockSize = Math.max(maxTextBlockSize, ((ColumnAttributesSubheader) subheader).sizeOfData());
+            }
+        }
+        write8(page, subheaderOffset + 8, maxTextBlockSize);
+
+        final int magicNumber = variables.size() == 1 ? 3 : 4;
+        //final int magicNumber = variables.size() - 1;
+        write8(page, subheaderOffset + 16, magicNumber); // unknown
+        write8(page, subheaderOffset + 24, 7); // unknown, maybe a count?
+        write8(page, subheaderOffset + 32, 0); // unknown
+        write8(page, subheaderOffset + 40, 0); // unknown
+        write8(page, subheaderOffset + 48, 0); // unknown
+        write8(page, subheaderOffset + 56, 0); // unknown
+        write8(page, subheaderOffset + 64, 0); // unknown
+        write8(page, subheaderOffset + 72, 0); // unknown
+        write8(page, subheaderOffset + 80, 0); // unknown
+        write8(page, subheaderOffset + 88, 0); // unknown
+        write8(page, subheaderOffset + 96, 0); // unknown
+        write8(page, subheaderOffset + 104, 0); // unknown
+        write8(page, subheaderOffset + 112, 1804); // unknown
+
+        writeSubheaderInformation(page, subheaderOffset + 120, ColumnAttributesSubheader.class, SIGNATURE_COLUMN_ATTRS);
+        writeSubheaderInformation(page, subheaderOffset + 160, ColumnTextSubheader.class, SIGNATURE_COLUMN_TEXT);
+        writeSubheaderInformation(page, subheaderOffset + 200, ColumnNameSubheader.class, SIGNATURE_COLUMN_NAME);
+        writeSubheaderInformation(page, subheaderOffset + 240, ColumnListSubheader.class, SIGNATURE_COLUMN_LIST);
+
+        // There are three subheader types that we don't know anything about.
+        writeSubheaderInformation(page, subheaderOffset + 280, null, SIGNATURE_UNKNOWN_A);
+        writeSubheaderInformation(page, subheaderOffset + 320, null, SIGNATURE_UNKNOWN_B);
+        writeSubheaderInformation(page, subheaderOffset + 360, null, SIGNATURE_UNKNOWN_C);
+
+        // There are five empty slots, possibly reserved for future use.
+        writeSubheaderInformation(page, subheaderOffset + 400, null, 0L);
+        writeSubheaderInformation(page, subheaderOffset + 440, null, 0L);
+        writeSubheaderInformation(page, subheaderOffset + 480, null, 0L);
+        writeSubheaderInformation(page, subheaderOffset + 520, null, 0L);
+        writeSubheaderInformation(page, subheaderOffset + 560, null, 0L);
+    }
+
+    @Override
+    byte typeCode() {
+        return SUBHEADER_TYPE_A;
+    }
+
+    @Override
+    byte compressionCode() {
+        return COMPRESSION_UNCOMPRESSED;
+    }
+}
---
+++ src/test/java/org/scharp/sas7bdat/SubheaderCountsSubheaderTest.java
@@ -0,0 +1,361 @@
+package org.scharp.sas7bdat;
+
+import org.junit.jupiter.api.Test;
+import org.scharp.sas7bdat.Sas7bdatWriter.Sas7bdatUnix64bitMetadata;
+import org.scharp.sas7bdat.Sas7bdatWriter.Sas7bdatUnix64bitVariables;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.scharp.sas7bdat.Subheader.COMPRESSION_UNCOMPRESSED;
+import static org.scharp.sas7bdat.Subheader.SUBHEADER_TYPE_A;
+
+/** Unit tests for {@link SubheaderCountsSubheader}. */
+public class SubheaderCountsSubheaderTest {
+
+    @Test
+    void testTypeCode() {
+        // Create a SubheaderCountsSubheader
+        PageSequenceGenerator pageSequenceGenerator = new PageSequenceGenerator();
+        List<Variable> variableList = List.of();
+        Sas7bdatUnix64bitVariables variables = new Sas7bdatUnix64bitVariables(variableList);
+        Sas7bdatUnix64bitMetadata metadata = new Sas7bdatUnix64bitMetadata(pageSequenceGenerator, 0x10000, variables);
+        SubheaderCountsSubheader subheaderCountsSubheader = new SubheaderCountsSubheader(variableList, metadata);
+
+        assertEquals(SUBHEADER_TYPE_A, subheaderCountsSubheader.typeCode());
+    }
+
+    @Test
+    void testCompressionCode() {
+        // Create a SubheaderCountsSubheader
+        PageSequenceGenerator pageSequenceGenerator = new PageSequenceGenerator();
+        List<Variable> variableList = List.of();
+        Sas7bdatUnix64bitVariables variables = new Sas7bdatUnix64bitVariables(variableList);
+        Sas7bdatUnix64bitMetadata metadata = new Sas7bdatUnix64bitMetadata(pageSequenceGenerator, 0x10000, variables);
+        SubheaderCountsSubheader subheaderCountsSubheader = new SubheaderCountsSubheader(variableList, metadata);
+
+        assertEquals(COMPRESSION_UNCOMPRESSED, subheaderCountsSubheader.compressionCode());
+    }
+
+    @Test
+    void basicTest() {
+        // Create a SubheaderCountsSubheader
+        List<Variable> variableList = List.of(
+            new Variable(
+                "VAR1",
+                1, // variable 1
+                VariableType.NUMERIC,
+                8,
+                "A label",
+                new Format("$OUTPUT", 8, 2),
+                new Format("$INPUT", 9, 6),
+                StrictnessMode.SAS_ANY),
+            new Variable(
+                "TEXT1",
+                2, // variable 2
+                VariableType.CHARACTER,
+                256,
+                "label",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY),
+            new Variable(
+                "LONGTEXT2", //
+                3, // variable 3
+                VariableType.CHARACTER,
+                101,
+                "label",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY),
+            new Variable(
+                "NUMBER 1", //
+                4, // variable 4
+                VariableType.NUMERIC,
+                8,
+                "label",
+                Format.UNSPECIFIED,
+                Format.UNSPECIFIED,
+                StrictnessMode.SAS_ANY));
+        PageSequenceGenerator pageSequenceGenerator = new PageSequenceGenerator();
+        Sas7bdatUnix64bitVariables variables = new Sas7bdatUnix64bitVariables(variableList);
+        Sas7bdatUnix64bitMetadata metadata = new Sas7bdatUnix64bitMetadata(pageSequenceGenerator, 0x10000, variables);
+        SubheaderCountsSubheader subheaderCountsSubheader = new SubheaderCountsSubheader(variableList, metadata);
+
+        // Add the subheaders that get counted to the metadata.
+        // This is intentionally not a realistically constructed metadata; many of the repeated
+        // subheaders are identical.  This helps to insulate this unit test from implementation changes
+        // to the other subheader types.
+
+        // Add two column text subheaders.
+        metadata.addSubheader(new ColumnTextSubheader((short) 0, (short) 500));
+        metadata.addSubheader(new ColumnTextSubheader((short) 1, (short) 500));
+
+        // Add one column attributes subheader
+        metadata.addSubheader(new ColumnAttributesSubheader(variables, 0, (short) 500));
+
+        // Add three column name subheaders
+        ColumnText columnText = new ColumnText(metadata);
+        metadata.addSubheader(new ColumnNameSubheader(variableList, 0, columnText));
+        metadata.addSubheader(new ColumnNameSubheader(variableList, 0, columnText));
+        metadata.addSubheader(new ColumnNameSubheader(variableList, 0, columnText));
+
+        // Add four column list subheaders
+        metadata.addSubheader(new ColumnListSubheader(variables, 0));
+        metadata.addSubheader(new ColumnListSubheader(variables, 0));
+        metadata.addSubheader(new ColumnListSubheader(variables, 0));
+        metadata.addSubheader(new ColumnListSubheader(variables, 0));
+
+        final byte[] expectedSubheaderData = new byte[] {
+            0, -4, -1, -1, -1, -1, -1, -1,  // signature
+
+            72, 0, 0, 0, 0, 0, 0, 0, // max subheader data size
+            4, 0, 0, 0, 0, 0, 0, 0, // unknown
+            7, 0, 0, 0, 0, 0, 0, 0, // unknown, maybe a count
+
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+
+            12, 7, 0, 0, 0, 0, 0, 0, // unknown
+
+            // ColumnAttributeSubheader information (offset 120)
+            -4, -1, -1, -1, -1, -1, -1, -1, // signature
+            1, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            3, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            1, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            3, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // ColumnTextSubheader information (offset 160)
+            -3, -1, -1, -1, -1, -1, -1, -1, // signature
+            1, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            1, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            1, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            2, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // ColumnNameSubheader information (offset 200)
+            -1, -1, -1, -1, -1, -1, -1, -1, // signature
+            1, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            4, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            1, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            6, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // ColumnListSubheader information (offset 240)
+            -2, -1, -1, -1, -1, -1, -1, -1, // signature
+            1, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            7, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            1, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            10, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // unknown A information (offset 280)
+            -5, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // unknown B information (offset 320)
+            -6, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // unknown C information (offset 360)
+            -7, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #1 information (offset 400)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #2 information (offset 440)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #3 information (offset 480)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #4 information (offset 520)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #5 information (offset 560)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+        };
+
+        int size = subheaderCountsSubheader.size();
+        assertEquals(expectedSubheaderData.length, subheaderCountsSubheader.size());
+
+        // Get the subheader with a non-zero offset.
+        int offset = 10;
+        byte[] actualSubheaderData = new byte[size + offset];
+        subheaderCountsSubheader.writeSubheader(actualSubheaderData, offset);
+
+        // Determine the expected return value.
+        byte[] expectedArray = new byte[expectedSubheaderData.length + offset];
+        System.arraycopy(expectedSubheaderData, 0, expectedArray, offset, expectedSubheaderData.length);
+
+        // Confirm that writeSubheader() wrote the data to the expected location.
+        assertArrayEquals(expectedArray, actualSubheaderData);
+    }
+
+    @Test
+    void testNoOtherSubheaders() {
+        // Create a SubheaderCountsSubheader
+        List<Variable> variableList = List.of(
+            new Variable(
+                "VAR1",
+                1, // variable 1
+                VariableType.NUMERIC,
+                8,
+                "A label",
+                new Format("$OUTPUT", 8, 2),
+                new Format("$INPUT", 9, 6),
+                StrictnessMode.SAS_ANY));
+        PageSequenceGenerator pageSequenceGenerator = new PageSequenceGenerator();
+        Sas7bdatUnix64bitVariables variables = new Sas7bdatUnix64bitVariables(variableList);
+        Sas7bdatUnix64bitMetadata metadata = new Sas7bdatUnix64bitMetadata(pageSequenceGenerator, 0x10000, variables);
+        SubheaderCountsSubheader subheaderCountsSubheader = new SubheaderCountsSubheader(variableList, metadata);
+
+        final byte[] expectedSubheaderData = new byte[] {
+            0, -4, -1, -1, -1, -1, -1, -1,  // signature
+
+            0, 0, 0, 0, 0, 0, 0, 0, // max subheader data size
+            3, 0, 0, 0, 0, 0, 0, 0, // unknown
+            7, 0, 0, 0, 0, 0, 0, 0, // unknown, maybe a count
+
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+            0, 0, 0, 0, 0, 0, 0, 0, // unknown
+
+            12, 7, 0, 0, 0, 0, 0, 0, // unknown
+
+            // ColumnAttributeSubheader information (offset 120)
+            -4, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // ColumnTextSubheader information (offset 160)
+            -3, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // ColumnNameSubheader information (offset 200)
+            -1, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // ColumnListSubheader information (offset 240)
+            -2, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // unknown A information (offset 280)
+            -5, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // unknown B information (offset 320)
+            -6, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // unknown C information (offset 360)
+            -7, -1, -1, -1, -1, -1, -1, -1, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #1 information (offset 400)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #2 information (offset 440)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #3 information (offset 480)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #4 information (offset 520)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+
+            // reserved #5 information (offset 560)
+            0, 0, 0, 0, 0, 0, 0, 0, // signature
+            0, 0, 0, 0, 0, 0, 0, 0, // page of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of first appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // page of last appearance
+            0, 0, 0, 0, 0, 0, 0, 0, // position of last appearance
+        };
+
+        assertEquals(expectedSubheaderData.length, subheaderCountsSubheader.size());
+
+        // Confirm that writeSubheader() wrote the data to the expected location.
+        byte[] actualSubheaderData = new byte[expectedSubheaderData.length];
+        subheaderCountsSubheader.writeSubheader(actualSubheaderData, 0);
+        assertArrayEquals(expectedSubheaderData, actualSubheaderData);
+    }
+}
