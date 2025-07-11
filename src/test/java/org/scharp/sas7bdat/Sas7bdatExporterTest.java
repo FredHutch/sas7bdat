c9eac2ded80b4eb0b9632d8f446f14374c41ccd0
David Costanzo <dcostanz@scharp.org>
2025-07-11 11:24:55 -0700

    Fix a test the name of a test file.

--- src/test/java/org/scharp/sas7bdat/Sas7bdatExporterTest.java
+++ src/test/java/org/scharp/sas7bdat/Sas7bdatExporterTest.java
@@ -520,7 +520,7 @@ public class Sas7bdatExporterTest {
 
     @Test
     public void testWriteObservationWithNullList() throws IOException {
-        Path targetPath = Path.of("testConstructWriteObservationWithNullList.sas7bdat");
+        Path targetPath = Path.of("testWriteObservationWithNullList.sas7bdat");
         Sas7bdatMetadata metadata = Sas7bdatMetadata.builder().
             variables(List.of(Variable.builder().name("TEXT").type(VariableType.CHARACTER).length(200).build())).
             build();
