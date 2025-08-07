e11f721687fa374352830791e542112cae08b2a5
David Costanzo <dcostanz@scharp.org>
2025-08-07 11:27:15 -0700

    Refactoring: Use the return value of PageReader.printSubheaderField8 to avoid duplicating the logic to get the value.

--- misc-scripts/show-sas7bdat.groovy
+++ misc-scripts/show-sas7bdat.groovy
@@ -534,8 +534,8 @@ void printPage(int fileOffset, int bitSize, byte[] page, ParsedState parsedState
                             break
 
                         case SubheaderSignature.COLUMN_ATTRS:
-                            pageReader.printSubheaderField2(subheaderOffset + 4, subheaderOffset + 8, "Length Remaining In Subheader")
-                            int totalVariables = (pageReader.readShort(subheaderOffset + 4, subheaderOffset + 8) - SIGNATURE_SIZE) / 16
+                            int payloadSize = pageReader.printSubheaderField2(subheaderOffset + 4, subheaderOffset + 8, "Length Remaining In Subheader")
+                            int totalVariables = (payloadSize - SIGNATURE_SIZE) / 16
                             for (int variableIndex = 0; variableIndex < totalVariables; variableIndex++) {
                                 int vectorOffset32 = subheaderOffset + 12 + variableIndex * 12
                                 int vectorOffset64 = subheaderOffset + 16 + variableIndex * 16
