8762fbab73add594bc2a98eb2443e5a0fb3e264a
David Costanzo <dcostanz@scharp.org>
2026-08-31 05:30:57 -0700

    Fix a typo in an exception message

--- misc-scripts/test-random-sas7bdat.groovy
+++ misc-scripts/test-random-sas7bdat.groovy
@@ -862,7 +862,7 @@ class TestRandomSas7bdat {
                     stringBuilder << formatNumericForDataline(variable, number)
 
                 } else {
-                    throw new RuntimeException("BUG: Formatting unsupported variable type in for a SAS datasline: $value.class")
+                    throw new RuntimeException("BUG: Formatting unsupported variable type in for a SAS dataline: $value.class")
                 }
             }
 
